9181a8a970b888b2ac2a07b7c5107239d162b1b6
partmant <ghdckdgml191@gmail.com>
2025-10-15 02:14:08 +0900

    test(StringSplitter): 문자열 분리 로직에 대한 테스트 추가
    - 기본 구분자(, :) 사용 시 문자열 분리
    - 커스텀 구분자(//;\n) 사용 시 문자열 분리
    - 커스텀 구분자가 숫자일 경우 예외 발생
    - 빈 값이 존재하는 경우 예외 발생
    - 커스텀 구분자와 기본 구분자 혼용 시 예외 발생

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -8,22 +8,50 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class ApplicationTest extends NsTest {
+	@Test
+	void 기본_구분자_사용() {
+		String input = "1,2:3";
+		String[] result = StringSplitter.split(input);
+		
+		assertThat(result).containsExactly("1", "2", "3");
+	}
+	
     @Test
     void 커스텀_구분자_사용() {
-        assertSimpleTest(() -> {
-            run("//;\\n1");
-            assertThat(output()).contains("결과 : 1");
-        });
+    	String input = "//;\n1;2;3";
+    	String[] result = StringSplitter.split(input);
+    	
+    	assertThat(result).containsExactly("1", "2", "3");
     }
-
+    
     @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
-        );
+    void 커스텀_구분자가_숫자이면_예외_처리() {
+    	String input = "//1\n,2,3";
+    	
+    	assertThatThrownBy(() -> StringSplitter.split(input))
+    		.isInstanceOf(IllegalArgumentException.class)
+    		.hasMessageContaining("숫자는 구분자로 사용할 수 없습니다.");
     }
-
+    
+    @Test
+    void 구분자로_빈_값이_있으면_예외_처리() {
+    	String input ="1,,2";
+    	
+    	assertThatThrownBy(() -> StringSplitter.split(input))
+    		.isInstanceOf(IllegalArgumentException.class)
+    		.hasMessageContaining("빈 값은 허용되지 않습니다.");
+    }
+    
+    @Test
+    void 커스텀_구분자와_기본_구분자를_혼용하면_예외_처리() {
+    	String input = "//;\n1,2;3";
+    	
+    	assertThatThrownBy(() -> StringSplitter.split(input))
+    		.isInstanceOf(IllegalArgumentException.class)
+    		.hasMessageContaining("기본 구분자는 사용할 수 없습니다.");
+    	
+    }
+    
     @Override
     public void runMain() {
         Application.main(new String[]{});
