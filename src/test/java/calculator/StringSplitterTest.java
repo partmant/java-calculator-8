3d61a3168914fdbf10e0e11ab333e6205eb8fdc2
partmant <ghdckdgml191@gmail.com>
2025-10-15 03:42:58 +0900

    refactor: StringSplitterTest 클래스로 분리
    - ApplicationTest에서 문자열 분리 테스트 코드 분리
    - 명확한 테스트를 위해 숫자 하나만 입력받는 테스트 코드 추가

---
+++ src/test/java/calculator/StringSplitterTest.java
@@ -0,0 +1,60 @@
+package calculator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.jupiter.api.Test;
+
+public class StringSplitterTest {
+	@Test
+	void 기본_구분자_사용() {
+		String input = "1,2:3";
+		String[] result = StringSplitter.split(input);
+		
+		assertThat(result).containsExactly("1", "2", "3");
+	}
+	
+    @Test
+    void 커스텀_구분자_사용() {
+    	String input = "//;\n1;2;3";
+    	String[] result = StringSplitter.split(input);
+    	
+    	assertThat(result).containsExactly("1", "2", "3");
+    }
+    
+    @Test
+    void 커스텀_구분자가_숫자이면_예외_처리() {
+    	String input = "//1\n,2,3";
+    	
+    	assertThatThrownBy(() -> StringSplitter.split(input))
+    		.isInstanceOf(IllegalArgumentException.class)
+    		.hasMessageContaining("숫자는 구분자로 사용할 수 없습니다.");
+    }
+    
+    @Test
+    void 구분자로_빈_값이_있으면_예외_처리() {
+    	String input ="1,,2";
+    	
+    	assertThatThrownBy(() -> StringSplitter.split(input))
+    		.isInstanceOf(IllegalArgumentException.class)
+    		.hasMessageContaining("빈 값은 허용되지 않습니다.");
+    }
+    
+    @Test
+    void 커스텀_구분자와_기본_구분자를_혼용하면_예외_처리() {
+    	String input = "//;\n1,2;3";
+    	
+    	assertThatThrownBy(() -> StringSplitter.split(input))
+    		.isInstanceOf(IllegalArgumentException.class)
+    		.hasMessageContaining("커스텀 구분자 사용 시 기본 구분자는 함께 사용할 수 없습니다.");
+    }
+    
+    @Test
+    void 숫자_하나만_입력되면_그_숫자를_반환() {
+    	String input = "3";
+    	
+    	String[] result = StringSplitter.split(input);
+    	
+    	assertThat(result).containsExactly("3");
+    }
+}
