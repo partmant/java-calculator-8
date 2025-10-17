d0f6dc8f6c988263c6cb38df9a887c893da12fb2
partmant <ghdckdgml191@gmail.com>
2025-10-18 03:47:49 +0900

    fix(InputView): 커스텀 구분자 입력 시 두 줄을 읽도록 수정

--- src/main/java/calculator/io/InputView.java
+++ src/main/java/calculator/io/InputView.java
@@ -5,6 +5,20 @@ import camp.nextstep.edu.missionutils.Console;
 public class InputView {
     public static String readInput() {
     	System.out.println("덧셈할 문자열을 입력해 주세요.");
-        return Console.readLine();
+
+        String firstLine = Console.readLine();
+
+        if (firstLine != null && firstLine.startsWith("//")) {
+            String secondLine = Console.readLine();
+            if (secondLine == null) {
+                secondLine = "";
+            }
+            return firstLine + "\n" + secondLine;
+        }
+        
+        if (firstLine == null) {
+        	return "";
+        }
+        return firstLine;
     }
 }
